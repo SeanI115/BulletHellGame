fd90c093c91281f5f7b8b2767179ce8311d14078
SeanI115 <Sinouye3@gmail.com>
2019-06-03 10:35:40 -1000

    Add files via upload

---
+++ src/path/Path.cs
@@ -0,0 +1,30 @@
+using BulletHell.GameEngine;
+using Microsoft.Xna.Framework;
+
+namespace BulletHell.path
+{
+    public abstract class Path
+    {
+
+        protected long StartTime;
+        protected Vector2 _initialLocation;
+        public Vector2 InitialLocation {get{return _initialLocation;}}
+
+        public abstract Vector2 UpdateLocation();
+
+        /*
+            Resets the start time to current time
+         */
+
+         public abstract Path Copy();
+        public void Reset()
+        {
+            this.StartTime = Clock.getClock().getTime();
+        }
+        public void ResetAt(Vector2 newStartingLocation)
+        {
+            _initialLocation = newStartingLocation;
+            Reset();
+        }
+    }
+}
